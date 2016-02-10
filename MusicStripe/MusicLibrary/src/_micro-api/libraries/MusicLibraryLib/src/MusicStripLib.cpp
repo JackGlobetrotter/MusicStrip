7b4603effa4d2b15fd152ec7bd0b411f05a2e12f
Jakob D. <jakob.dickert@gmx.de>
2016-02-10 09:23:38 +0100

    MusicStripLib

---
+++ MusicStripe/MusicLibrary/src/_micro-api/libraries/MusicLibraryLib/src/MusicStripLib.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		MusicLibraryLib.cpp
+ Created:	10/02/2016 09:10:50
+ Author:	Jakob D.
+
+*/
+
+#include "MusicStripLib.h"
+
+
---
+++ MusicStripe/MusicLibrary/src/_micro-api/libraries/MusicLibraryLib/src/MusicStripLib.h
@@ -0,0 +1,42 @@
+/*
+ Name:		MusicLib.h
+ Created:	10/02/2016 09:10:50
+ Author:	Jakob D.
+
+*/
+
+#ifndef _MusicStripLib_h
+#define _MusicStripLib_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+enum ControlByte : uint8_t {
+	RGBColor = 1,
+	MusicTitle,
+
+	SwitchStade,
+	Start,
+	Stop,
+	LightToggle,
+	IP,
+	Port,
+	SSID,
+	PWD,
+	Connect,
+	Disconnect,
+	MusicState,
+	MicroState,
+	FadeState,
+	SmoothState,
+	FlashState,
+	FixColorState,
+	WifiConnectSate
+
+};
+
+#endif
+
